3dd9a7c385eda65a4eafc3e5037dceb4ab106af7
Callum Mellor-Reed <me@callummr.com>
2016-05-18 20:02:08 +0100

    Add basket HTML to component

--- src/components/basket.js
+++ src/components/basket.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 
 export default class Basket extends Component {
   render() {
-    return <p>Basket</p>
+    return (
+      <aside className="basket">
+        <h1>Basket</h1>
+
+        <table className="products">
+          <thead>
+            <tr>
+              <th>Product name</th>
+              <th>qty</th>
+            </tr>
+          </thead>
+
+          <tbody></tbody>
+        </table>
+
+        <div className="total">
+          <span className="label">Total: </span>
+          <span className="amount">£0.00</span>
+        </div>
+      </aside>
+    )
   }
 }
