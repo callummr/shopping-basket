fbfa288d5688e6954b9197263cf0884ce7d15d35
Callum Mellor-Reed <me@callummr.com>
2016-05-18 20:30:19 +0100

    Basket reducer first pass

--- src/reducers/basket.js
+++ src/reducers/basket.js
@@ -1,3 +1,32 @@
-export default (state = [], action) => {
-  return state
+import { combineReducers } from 'redux'
+import { ADD_TO_BASKET } from './actionTypes'
+
+const basketIds = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TO_BASKET:
+      if (state.indexOf(action.id) > -1) {
+        return [...state, id]
+      } else {
+        return state
+      }
+    default:
+      return state
+  }
 }
+
+const basketQuantities = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_TO_BASKET:
+      const quantity = state[action.id] || 0
+      return Object.assign({...state}, {
+        [action.id]: quantity + 1
+      })
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  ids: basketIds,
+  quantities: basketQuantities
+})
